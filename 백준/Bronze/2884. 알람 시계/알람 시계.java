068fc2ca4dca77767e61123b96aefc27feb5f4b0
Saem Justina Hyun <107621181+SaemHyun@users.noreply.github.com>
2022-07-13 14:29:29 +0900

    [Bronze III] Title: 알람 시계, Time: 240 ms, Memory: 18472 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+
+        if( M < 45){
+            H--;
+            M= 60 - (45 - M);
+            if(H<0){
+                H=23;
+            }
+            System.out.println(H+" "+M);
+            }else{
+            System.out.println(H+" "+(M-45));
+        }
+        sc.close();
+    }
+}
